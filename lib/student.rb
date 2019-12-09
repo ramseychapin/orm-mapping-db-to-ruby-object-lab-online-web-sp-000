ef01a225e6cc75745d0e2badabc7dbfa997752f8
Ramsey Chapin <ramsey@ramseychapin.com>
2019-12-09 02:14:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,9 @@ class Student
     LIMIT ?
     SQL
 
-    DB[:conn].execute(sql, x)
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_X_students_in_grade_10(x)
