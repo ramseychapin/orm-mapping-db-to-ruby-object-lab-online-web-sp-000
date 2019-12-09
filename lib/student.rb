1249f7ad1fe7ff563ce45854ca2987cac38887aa
Ramsey Chapin <ramsey@ramseychapin.com>
2019-12-09 02:24:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,6 +83,18 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
