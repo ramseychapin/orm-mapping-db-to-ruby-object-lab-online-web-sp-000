8da25c843e4b3c69485169aa8c37c9cc81968ad6
Ramsey Chapin <ramsey@ramseychapin.com>
2019-12-09 02:20:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,17 +70,6 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(x)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade == 10
-      LIMIT ?
-    SQL
-
-    DB[:conn].execute(sql, x)
-  end
-
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
