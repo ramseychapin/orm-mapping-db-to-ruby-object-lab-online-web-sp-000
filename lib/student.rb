d6f53f2edfefb8b947cfad219e0a270bd01569e8
Ramsey Chapin <ramsey@ramseychapin.com>
2019-12-09 01:53:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
       WHERE grade < 12
     SQL
 
-    DB[:conn].execute(sql,name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.
   end
