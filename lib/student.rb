00e28863753fcb5a4280042f580f7bd297a9b57b
Ramsey Chapin <ramsey@ramseychapin.com>
2019-12-09 02:18:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,11 +59,10 @@ class Student
 
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE grade = 10
-    ORDER BY students.id
-    LIMIT ?
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
     SQL
 
     DB[:conn].execute(sql, x).map do |row|
